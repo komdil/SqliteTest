ee225abe57151ee21ecee9404f6e6a89876b5ff7
Komilov Dilshod <komilovd@silkroadprofessionals.com>
2022-04-14 09:20:00 +0500

    add same path

--- SqliteTest/Program.cs
+++ SqliteTest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using SqliteTest;
-using (SQliteDBContext context = new SQliteDBContext("MyTest.db"))
+using (SQliteDBContext context = new SQliteDBContext(SubscriberCacheConstants.GetDatabaseName()))
 {
     context.Database.EnsureCreated();
+    Console.ReadLine();
 }
--- SqliteTest/SQliteDBContext.cs
+++ SqliteTest/SQliteDBContext.cs
@@ -9,23 +9,6 @@ namespace SqliteTest
 {
     internal class SQliteDBContext : DbContext
     {
-        static DirectoryInfo GetCachePath()
-        {
-            var commonFolder = Environment.SpecialFolder.CommonApplicationData;
-            var commonPath = Environment.GetFolderPath(commonFolder);
-            return new DirectoryInfo(Path.Combine(commonPath, "Eagle2", "Cache"));
-        }
-
-        static string GetDatabasePath(string databaseName)
-        {
-            DirectoryInfo commonCachePath = GetCachePath();
-            if (!commonCachePath.Exists)
-                commonCachePath.Create();
-
-            var databasePath = Path.Combine(commonCachePath.FullName, $"{databaseName}.db");
-            return databasePath;
-        }
-
         public string DatabaseName { get; }
 
         public SQliteDBContext(string databaseName)
@@ -35,8 +18,8 @@ namespace SqliteTest
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            string databasePath = GetDatabasePath(DatabaseName);
-            optionsBuilder.UseSqlite($"Data Source={databasePath}");
+            string databasePath = SubscriberCacheConstants.GetDatabasePath(DatabaseName);
+            optionsBuilder.UseSqlite($"FileName={databasePath}");
             base.OnConfiguring(optionsBuilder);
         }
 
@@ -52,5 +35,44 @@ namespace SqliteTest
         public Guid Guid { get; set; }
 
         public string Name { get; set; }
+    }
+
+    public class SubscriberCacheConstants
+    {
+        public static DirectoryInfo GetCachePath()
+        {
+            var commonFolder = Environment.SpecialFolder.ApplicationData; //containers don't have access to write to common files
+            var commonPath = Environment.GetFolderPath(commonFolder);
+            return new DirectoryInfo(Path.Combine(commonPath, "Eagle3", "Cache"));
+        }
+
+        public static string GetDatabasePath(string databaseName)
+        {
+            DirectoryInfo commonCachePath = GetCachePath();
+            if (!commonCachePath.Exists)
+                commonCachePath.Create();
+
+            var databasePath = Path.Combine(commonCachePath.FullName, $"{databaseName}.db");
+            return databasePath;
+        }
+
+        public static string GetDatabaseNameRoot()
+        {
+            string mode = "RELEASE";
+#if DEBUG 
+            mode = "DEBUG";
+#endif
+            return $"{mode}_Vision_Demo_";
+        }
+
+        public static string GetDatabaseName(string apiVersion = "")
+        {
+            if (string.IsNullOrEmpty(apiVersion))
+                apiVersion = "1.1.1.1";
+
+            apiVersion = apiVersion.Replace('.', '_'); //need to add the api version so that when there are two servers running they have different cache files
+            var root = GetDatabaseNameRoot();
+            return $"{root}_{apiVersion}_Vision_Demo";
+        }
     }
 }
